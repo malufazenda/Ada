6e5b7e602c2779977c894e96439992cb5dff92ee
malufazenda <malu.fazenda@gmail.com>
2025-05-07 19:19:32 -0300

    Update Malu_Fazenda.py

---
+++ Soluções/Malu_Fazenda.py
@@ -0,0 +1,8 @@
+# 1. Crie um dicionário chamado aluno com as chaves "nome", "idade" e "nota". 
+# Atribua valores fictícios e imprima cada valor individualmente.
+
+aluno = {"nome":"Malu", "idade":27, "nota":8}
+
+print(aluno["nome"])
+print(aluno["idade"])
+print(aluno["nota"])
